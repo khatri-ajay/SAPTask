a12bf8b55c2d83fdfadd45079482ac085e59d709
Ajay Kumar <ajaykhatri2123@Outlook.com>
2022-02-03 00:10:54 +0100

    Search Bar and Images Table View Created.

--- SAPTask/Controllers/DashboardVC/DashboardVC.swift
+++ SAPTask/Controllers/DashboardVC/DashboardVC.swift
@@ -6,9 +6,22 @@
 //
 
 import UIKit
+import PrettyConstraints
 
 class DashboardVC: UIViewController {
-
+    
+    // MARK: - UI Object creation
+    private var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }()
+    private let imagesTableView: UITableView = {
+        let tableview = UITableView()
+//        tableview.register(MovieTableViewCell.self, forCellReuseIdentifier: cellIdentifier.MovieTableViewCell)
+        tableview.backgroundColor = .white
+        tableview.separatorStyle = .none
+        return tableview
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
