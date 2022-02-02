98186833cef702a0653e790e14015f319741814a
Ajay Kumar <ajaykhatri2123@Outlook.com>
2022-02-03 00:01:11 +0100

    Project Setup

--- SAPTask/Controllers/DashboardVC/DashboardVC.swift
+++ SAPTask/Controllers/DashboardVC/DashboardVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DashboardVC.swift
 //  SAPTask
 //
 //  Created by Ajay Kumar on 02/02/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DashboardVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
